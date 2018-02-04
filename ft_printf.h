e57b1112e26abb0692e084ff6715182251812b46
Mykhailo <0660330233@ukr.net>
2018-02-04 19:16:20 +0200

    work with D

--- ft_printf.h
+++ ft_printf.h
@@ -19,10 +19,29 @@
 # include <unistd.h>
 # include <stdarg.h>
 # include <string.h>
+#include <inttypes.h>
+#include <stddef.h> 
 
+typedef struct 	s_list
+{
+	char		*flags;
+	char		*width;
+	char		*precision;
+	char		*size;
+	char 		specificator;
+}				t_list;
 void		ft_putchar(char c);
 void		ft_putstr(char const *s);
 void		ft_putnbr(int n);
 int			is_input_spec(char ch);
-void		search_specificator(char *str);
+size_t		ft_strlen(const char *s);
+char		*ft_strjoin(char const *s1, char const *s2);
+void		*ft_memalloc(size_t size);
+char		*ft_strncpy(char *dest, const char *src, size_t n);
+char		*ft_itoa(size_t n);
+void		ft_bzero(void *s, size_t n);
+int			ft_atoi(const char *nptr);
+int 		ft_isdigit(int character);
+int			ft_is_it_space(char ch);
+int			ft_strcmp(const char *s1, const char *s2);
 #endif
